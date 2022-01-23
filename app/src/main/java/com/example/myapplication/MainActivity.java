134baed1b44a4d470b0d7d969d6b09ec6f70df0e
PP <ppzaq1pw@gmail.com>
2022-01-23 11:54:36 +0200

    Init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+//    TextView myText;
+//    View myButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        myText = findViewById(R.id.myText);
+//        myButton = findViewById(R.id.myButton);
+//        myButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                myText.setText("HELLO BRO");
+//            }
+//        });
+    }
+}
